e3c7a8463871f496c88db8676c36332942faa0fd
agr3w <suzanakampa.sk@gmail.com>
2026-06-11 09:12:35 -0300

    TD-52 Adicionando sessao de sobre na propria home

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -3,6 +3,7 @@ import Hero from "@sections/home/Hero";
 import FeaturedProperties from "@sections/home/FeaturedProperties";
 import Stats from "@sections/home/Stats";
 import Contact from "@sections/home/Contact";
+import HomeAbout from "@sections/home/HomeAbout.jsx";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -16,6 +17,7 @@ export default function Home() {
       <Hero />
       <Stats />
       <FeaturedProperties onPropertyClick={handlePropertyClick} />
+      <HomeAbout />
       <Contact />
     </main>
   );
